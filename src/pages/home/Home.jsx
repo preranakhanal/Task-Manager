6c184a8ebc2ff19497481e417acd1f3a650307f3
preranakhanal <preranakhanal42@gmail.com>
2025-09-13 20:35:56 +0545

    feat: update dependencies and remove unused files
    - Added @emotion/react, @emotion/styled, @mui/icons-material, @mui/material, and sass to package.json
    - Removed favicon.ico, logo192.png, logo512.png, logo.svg, App.css, App.test.js, index.css, reportWebVitals.js, and setupTests.js
    - Updated index.html to include Google Fonts and reset styles
    - Refactored App.js to use Home component
    - Created Sidebar component with basic structure and styles
    - Created Home component to include Sidebar

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.scss'
+import Sidebar from '../../components/sidebar/Sidebar'
+
+
+const Home = () => {
+  return (
+    <div>
+        <Sidebar />
+    </div>
+  )
+}
+
+export default Home
